bf4531cc196af70fe2b9706c3368133ebc74f346
Petr Bouda <bouda.petr@gmail.com>
2024-02-05 08:47:59 +0100

    28: Rewrite flamegraph to a separated Javascript file (#29)
    * 28: Start
    
    * 28: Rewrite flamegraph to separated javascript file

---
+++ pages/src/service/Flamegraph.js
@@ -0,0 +1,254 @@
+export default class Flamegraph {
+
+    static FRAME_HEIGHT = 20;
+    static FRAME_HEIGHT_2 = 21;
+
+    depth = null;
+
+    // set up by draw function
+    canvasHeight = null;
+    canvasWidth = null;
+    currentRoot = null;
+    currentRootLevel = null;
+    pxPerSample = null
+
+    reversed = false;
+
+    visibleFrames = [];
+
+    constructor(data, canvas, hl, status) {
+        this.hl = hl;
+        this.status = status;
+        this.depth = data.depth;
+        this.levels = data.levels;
+        this.currentRoot = this.levels[0][0];
+        this.currentRootLevel = 0;
+        this.currentPattern = null;
+        this.canvas = canvas;
+        this.context = canvas.getContext('2d');
+        this.visibleFrames = Flamegraph.initializeLevels(this.depth);
+
+        this.resizeCanvas(canvas.offsetWidth, canvas.offsetHeight);
+
+        this.canvas.onmousemove = this.#onMouseMoveEvent();
+        this.canvas.onmouseout = this.#onMouseOut();
+        this.canvas.ondblclick = this.#onDoubleClick();
+    }
+
+    #onMouseMoveEvent() {
+        return () => {
+            const level = Math.floor((this.reversed ? event.offsetY : this.canvasHeight - event.offsetY) / Flamegraph.FRAME_HEIGHT_2);
+
+            if (level >= 0 && level < this.levels.length) {
+                let frame = this.#lookupFrame(level);
+
+                if (frame) {
+                    if (frame !== this.currentRoot) {
+                        getSelection().removeAllRanges();
+                    }
+
+                    this.hl.style.left = Math.max(frame.left - this.currentRoot.left, 0) * this.pxPerSample + this.canvas.offsetLeft + 'px';
+                    this.hl.style.width = Math.min(frame.width, this.currentRoot.width) * this.pxPerSample  + 'px';
+                    this.hl.style.top = (this.reversed ? level * Flamegraph.FRAME_HEIGHT_2 : this.canvasHeight - (level + 1) * Flamegraph.FRAME_HEIGHT_2) + this.canvas.offsetTop + 'px';
+                    this.hl.firstChild.textContent = frame.title;
+                    this.hl.style.display = 'block';
+
+                    this.canvas.title = frame.title + '\n(' + Flamegraph.#samples(frame.width) + frame.details + ', ' + Flamegraph.#pct(frame.width, this.levels[0][0].width) + '%)';
+                    this.canvas.style.cursor = 'pointer';
+                    this.canvas.onclick = () => {
+                        if (frame !== this.currentRoot) {
+                            this.#draw(frame, level, this.currentPattern);
+                            this.canvas.onmousemove();
+                        }
+                    };
+                    this.status.textContent = this.canvas.title;
+                    this.status.style.display = 'inline-block';
+                    this.status.style.left = document.getElementById('layout-main').getBoundingClientRect().left + 'px';
+                    this.status.style.bottom = this.status.style.bottom + 10 + 'px';
+                    return;
+                }
+
+                this.canvas.onmouseout();
+            }
+        };
+    }
+
+    #onMouseOut() {
+        return () => {
+            this.hl.style.display = 'none';
+            this.status.style.display = 'none';
+            this.canvas.title = '';
+            this.canvas.style.cursor = '';
+            this.canvas.onclick = '';
+        };
+    };
+
+    #onDoubleClick() {
+        return () => {
+            getSelection().selectAllChildren(this.hl);
+        };
+    };
+
+    static #samples(n) {
+        return n === 1 ? '1 sample' : n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + ' samples';
+    }
+
+    static #pct(a, b) {
+        return a >= b ? '100' : ((100 * a) / b).toFixed(2);
+    }
+
+    #lookupFrame(level) {
+        let frames = this.visibleFrames[level];
+        for (let i = 0; i < frames.length; i++) {
+            let visibleFrame = frames[i];
+
+            if (this.context.isPointInPath(Flamegraph.#toPath2D(visibleFrame.rect), event.offsetX, event.offsetY)) {
+                return visibleFrame.frame;
+            }
+        }
+    }
+
+    resizeCanvas(width, height) {
+        if (height != null) {
+            this.canvasHeight = height;
+            this.canvas.height = this.canvasHeight * (devicePixelRatio || 1);
+        }
+
+        this.canvasWidth = width;
+        this.canvas.style.width = this.canvasWidth + 'px';
+
+        this.canvas.width = this.canvasWidth * (devicePixelRatio || 1);
+        if (devicePixelRatio) {
+            this.context.scale(devicePixelRatio, devicePixelRatio);
+        }
+        this.context.font = '12px Arial';
+        this.drawRoot();
+    }
+
+    static initializeLevels(depth) {
+        let levels = Array(depth + 1);
+        for (let h = 0; h < levels.length; h++) {
+            levels[h] = [];
+        }
+        return levels;
+    }
+
+    clearCanvas() {
+        this.context.fillStyle = '#ffffff';
+        this.context.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+    }
+
+    drawRoot() {
+        this.#draw(this.currentRoot, this.currentRootLevel, this.currentPattern);
+    }
+
+    reverse() {
+        this.reversed = !this.reversed;
+        this.#draw(this.currentRoot, this.currentRootLevel, this.currentPattern);
+    }
+
+    search(pattern) {
+        this.currentPattern = pattern;
+        let highlighted = this.#draw(this.currentRoot, this.currentRootLevel, this.currentPattern);
+        let highlightedTotal = Flamegraph.#calculateHighlighted(highlighted);
+        return Flamegraph.#pct(highlightedTotal, this.currentRoot.width);
+    }
+
+    resetSearch() {
+        this.currentPattern = null;
+        this.#draw(this.currentRoot, this.currentRootLevel, this.currentPattern);
+    }
+
+    #draw(root, rootLevel, pattern) {
+        this.clearCanvas();
+        this.visibleFrames = Flamegraph.initializeLevels(this.depth);
+
+        this.currentRoot = root;
+        this.currentRootLevel = rootLevel;
+
+        this.pxPerSample = this.canvasWidth / root.width;
+
+        const xStart = root.left;
+        const xEnd = xStart + root.width;
+        const highlighted = []
+
+        for (let level = 0; level < this.levels.length; level++) {
+            const y = this.reversed ? level * (Flamegraph.FRAME_HEIGHT + 1) : this.canvasHeight - (level + 1) * (Flamegraph.FRAME_HEIGHT_2);
+            const frames = this.levels[level];
+
+            for (let i = 0; i < frames.length; i++) {
+                let frame = frames[i];
+                if (Flamegraph.#frame_not_overflow(frame, xStart, xEnd)) {
+                    let isHighlighted = Flamegraph.#isMethodHighlighted(highlighted, frame, pattern);
+                    let isUnderRoot = level < rootLevel;
+
+                    const rectangle = this.#createRectangle(this.pxPerSample, frame, y, xStart);
+                    this.visibleFrames[level].push({ rect: rectangle, frame: frame });
+                    this.#drawFrame(this.pxPerSample, frame, y, xStart, rectangle, isHighlighted, isUnderRoot);
+                }
+            }
+        }
+
+        return highlighted
+    }
+
+    static #frame_not_overflow(frame, xStart, xEnd) {
+        return frame.left < xEnd && frame.left + frame.width > xStart;
+    }
+
+    static #highlight(highlighted, frame) {
+        return highlighted[frame.left] >= frame.width || (highlighted[frame.left] = frame.width)
+    }
+
+    static #isMethodHighlighted(highlighted, frame, pattern) {
+        return pattern && frame.title.match(pattern) && Flamegraph.#highlight(highlighted, frame);
+    }
+
+    static #calculateHighlighted(highlighted) {
+        let total = 0;
+        let left = 0;
+        Object.keys(highlighted)
+            .sort(function(a, b) {
+                return a - b;
+            })
+            .forEach(function(x) {
+                if (+x >= left) {
+                    total += highlighted[x];
+                    left = +x + highlighted[x];
+                }
+            });
+        return total;
+    }
+
+    static #toPath2D(rect) {
+        const path = new Path2D()
+        path.rect(rect.x, rect.y, rect.width, rect.height)
+        return path;
+    }
+
+    #createRectangle(pxPerSample, frame, y, xStart) {
+        const x = (frame.left - xStart) * pxPerSample;
+        const width = frame.width * pxPerSample;
+        return { x: x, y: y, width: width, height: Flamegraph.FRAME_HEIGHT };
+    }
+
+    #drawFrame(pxPerSample, frame, y, xStart, rect, isHighlighted, isUnderRoot) {
+        const path = Flamegraph.#toPath2D(rect)
+
+        this.context.fillStyle = isHighlighted ? '#ee00ee' : frame.color;
+        this.context.fill(path);
+
+        // Do we want to fill the text, or the frame is too small and leave it empty
+        if (frame.width * pxPerSample >= 21) {
+            const chars = Math.floor((frame.width * pxPerSample) / 7);
+            const title = frame.title.length <= chars ? frame.title : frame.title.substring(0, chars - 2) + '..';
+            this.context.fillStyle = '#000000';
+            this.context.fillText(title, Math.max(frame.left - xStart, 0) * pxPerSample + 3, y + 14, frame.width * pxPerSample - 6);
+        }
+
+        if (isUnderRoot) {
+            this.context.fillStyle = 'rgba(255, 255, 255, 0.5)';
+            this.context.fill(path);
+        }
+    }
+}
--- pages/src/views/flamegraph/Show.vue
+++ pages/src/views/flamegraph/Show.vue
@@ -8,92 +8,33 @@ import { onMounted, ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import GlobalVars from '@/service/GlobalVars';
 import { useToast } from 'primevue/usetoast';
+import Flamegraph from '@/service/Flamegraph';
 
-let root, rootLevel, px, pattern;
-let reverse = false;
-let hl, status, levels, c, canvas, canvasWidth, canvasHeight;
 const router = useRouter();
 const route = useRoute();
 const selectedEventType = ref(null);
 const jfrEventTypes = ref(GlobalVars.jfrTypes());
 const toast = useToast();
 
+let hl, status, canvas;
+let flamegraph = null;
 
 function onResize({ width, height }) {
     // minus padding
-    setCanvasWidth(width - 50);
-
-    render();
-}
-
-function setCanvasWidth(width) {
-    canvasWidth = width;
-    canvas.style.width = canvasWidth + 'px';
-
-    canvas.width = canvasWidth * (devicePixelRatio || 1);
-    if (devicePixelRatio) c.scale(devicePixelRatio, devicePixelRatio);
-    c.font = '12px Arial';
+    flamegraph.resizeCanvas(width - 50);
 }
 
 onMounted(() => {
-    selectedEventType.value = jfrEventTypes.value[route.query.eventType]
-
-    f(0, 0, 0, 0, 'fake-to-include-unused-method');
+    selectedEventType.value = jfrEventTypes.value[route.query.eventType];
 
     canvas = document.getElementById('canvas');
-    c = canvas.getContext('2d');
     hl = document.getElementById('hl');
     status = document.getElementById('status');
 
-    canvasHeight = canvas.offsetHeight;
-    canvas.height = canvasHeight * (devicePixelRatio || 1);
-
-    setCanvasWidth(canvas.offsetWidth);
-
-    canvas.onmousemove = function() {
-        const h = Math.floor((reverse ? event.offsetY : canvasHeight - event.offsetY) / 16);
-        if (h >= 0 && h < levels.length) {
-            const f = findFrame(levels[h], event.offsetX / px + root.left);
-            if (f) {
-                if (f !== root) getSelection().removeAllRanges();
-                hl.style.left = Math.max(f.left - root.left, 0) * px + canvas.offsetLeft + 'px';
-                hl.style.width = Math.min(f.width, root.width) * px + 'px';
-                hl.style.top = (reverse ? h * 16 : canvasHeight - (h + 1) * 16) + canvas.offsetTop + 'px';
-                hl.firstChild.textContent = f.title;
-                hl.style.display = 'block';
-                canvas.title = f.title + '\n(' + samples(f.width) + f.details + ', ' + pct(f.width, levels[0][0].width) + '%)';
-                canvas.style.cursor = 'pointer';
-                canvas.onclick = function() {
-                    if (f !== root) {
-                        render(f, h);
-                        canvas.onmousemove();
-                    }
-                };
-                status.textContent = canvas.title;
-                status.style.display = 'inline-block';
-                status.style.left = document.getElementById('layout-main').getBoundingClientRect().left + 'px';
-                status.style.bottom = status.style.bottom + 10 + 'px';
-                return;
-            }
-        }
-        canvas.onmouseout();
-    };
-
-    canvas.onmouseout = function() {
-        hl.style.display = 'none';
-        status.style.display = 'none';
-        canvas.title = '';
-        canvas.style.cursor = '';
-        canvas.onclick = '';
-    };
-
-    canvas.ondblclick = function() {
-        getSelection().selectAllChildren(hl);
-    };
+    updateFlamegraph(canvas, route.query.eventType);
 
     document.getElementById('reverse').onclick = function() {
-        reverse = !reverse;
-        render();
+        flamegraph.reverse();
     };
 
     document.getElementById('search').onclick = function() {
@@ -112,113 +53,49 @@ onMounted(() => {
             search(false);
         }
     };
-
-    updateFlamegraph(route.query.eventType)
 });
 
-function updateFlamegraph(eventType) {
+function updateFlamegraph(canvas, eventType) {
     let flamegraphMode = route.query.mode;
     if (flamegraphMode === 'predefined') {
         FlamegraphService.getPredefined(route.query.profileId, jfrEventTypes.value[eventType].code)
-            .then((data) => processIncomingData(data))
-            .then(() => render())
-            .then(() => search());
+            .then((data) => {
+                flamegraph = new Flamegraph(data, canvas, hl, status);
+                flamegraph.drawRoot();
+            });
     } else {
         FlamegraphService.getSingle(route.query.profileId, route.query.flamegraphId)
-            .then((data) => processIncomingData(data))
-            .then(() => render())
-            .then(() => search());
+            .then((data) => {
+                flamegraph = new Flamegraph(data, canvas, hl, status);
+                flamegraph.drawRoot();
+            });
     }
 }
 
 const clickEventTypeSelected = () => {
     let eventTypeIndex = selectedEventType.value.index;
-    router.push({ name: 'flamegraph-show', query: { mode: 'predefined', profileId: route.query.profileId, eventType: jfrEventTypes.value[eventTypeIndex].code } });
-    updateFlamegraph(eventTypeIndex)
-};
-
-const palette = [
-    [0xb2e1b2, 20, 20, 20],
-    [0x50e150, 30, 30, 30],
-    [0x50cccc, 30, 30, 30],
-    [0xe15a5a, 30, 40, 40],
-    [0xc8c83c, 30, 30, 10],
-    [0xe17d00, 30, 30, 0],
-    [0xcce880, 20, 20, 20]
-];
-
-function processIncomingData(data) {
-    updateLevels(data.depth);
-    data.frames.forEach((line) => {
-        eval(line);
-    });
-}
-
-function getColor(p) {
-    const v = Math.random();
-    return '#' + (p[0] + (((p[1] * v) << 16) | ((p[2] * v) << 8) | (p[3] * v))).toString(16);
-}
-
-function f(level, left, width, type, title, inln, c1, int) {
-    if (level === 0 && left === 0 && width === 0 && type === 0 && title === 'fake-to-include-unused-method') {
-        return;
-    }
-
-    levels[level].push({
-        left: left,
-        width: width,
-        color: getColor(palette[type]),
-        title: title,
-        details: (int ? ', int=' + int : '') + (c1 ? ', c1=' + c1 : '') + (inln ? ', inln=' + inln : '')
-    });
-}
-
-function updateLevels(numOfLevels) {
-    levels = Array(numOfLevels + 1);
-    for (let h = 0; h < levels.length; h++) {
-        levels[h] = [];
-    }
-}
-
-function samples(n) {
-    return n === 1 ? '1 sample' : n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + ' samples';
-}
-
-function pct(a, b) {
-    return a >= b ? '100' : ((100 * a) / b).toFixed(2);
-}
-
-function findFrame(frames, x) {
-    let left = 0;
-    let right = frames.length - 1;
-
-    while (left <= right) {
-        const mid = (left + right) >>> 1;
-        const f = frames[mid];
-
-        if (f.left > x) {
-            right = mid - 1;
-        } else if (f.left + f.width <= x) {
-            left = mid + 1;
-        } else {
-            return f;
+    router.push({ name: 'flamegraph-show',
+        query: {
+            mode: 'predefined',
+            profileId: route.query.profileId,
+            eventType: jfrEventTypes.value[eventTypeIndex].code
         }
-    }
-
-    if (frames[left] && (frames[left].left - x) * px < 0.5) return frames[left];
-    if (frames[right] && (x - (frames[right].left + frames[right].width)) * px < 0.5) return frames[right];
-
-    return null;
-}
+    });
+    updateFlamegraph(eventTypeIndex);
+};
 
 function search(r) {
     if (r === true && (r = prompt('Enter regexp to search:', '')) === null) {
         return;
     }
 
-    pattern = r ? RegExp(r) : undefined;
-    const matched = render(root, rootLevel);
-    document.getElementById('matchval').textContent = pct(matched, root.width) + '%';
+    let pattern = r ? RegExp(r) : undefined;
+    if (pattern) {
+        const matched = flamegraph.search(pattern);
+        document.getElementById('matchval').textContent = matched + '%';
+    } else {
+        flamegraph.resetSearch();
+    }
 
     const matchEl = document.getElementById('match');
     matchEl.style.display = r ? 'inline-block' : 'none';
@@ -229,75 +106,10 @@ function search(r) {
     matchEl.style.left = xPosition + 'px';
 }
 
-function render(newRoot, newLevel) {
-    if (root) {
-        c.fillStyle = '#ffffff';
-        c.fillRect(0, 0, canvasWidth, canvasHeight);
-    }
-
-    root = newRoot || levels[0][0];
-    rootLevel = newLevel || 0;
-    px = canvasWidth / root.width;
-
-    const x0 = root.left;
-    const x1 = x0 + root.width;
-    const marked = [];
-
-    function mark(f) {
-        return marked[f.left] >= f.width || (marked[f.left] = f.width);
-    }
-
-    function totalMarked() {
-        let total = 0;
-        let left = 0;
-        Object.keys(marked)
-            .sort(function(a, b) {
-                return a - b;
-            })
-            .forEach(function(x) {
-                if (+x >= left) {
-                    total += marked[x];
-                    left = +x + marked[x];
-                }
-            });
-        return total;
-    }
-
-    function drawFrame(f, y, alpha) {
-        if (f.left < x1 && f.left + f.width > x0) {
-            c.fillStyle = pattern && f.title.match(pattern) && mark(f) ? '#ee00ee' : f.color;
-            c.fillRect((f.left - x0) * px, y, f.width * px, 15);
-
-            if (f.width * px >= 21) {
-                const chars = Math.floor((f.width * px) / 7);
-                const title = f.title.length <= chars ? f.title : f.title.substring(0, chars - 2) + '..';
-                c.fillStyle = '#000000';
-                c.fillText(title, Math.max(f.left - x0, 0) * px + 3, y + 12, f.width * px - 6);
-            }
-
-            if (alpha) {
-                c.fillStyle = 'rgba(255, 255, 255, 0.5)';
-                c.fillRect((f.left - x0) * px, y, f.width * px, 15);
-            }
-        }
-    }
-
-    // eslint-disable-next-line prettier/prettier
-    for (let h = 0; h < levels.length; h++) {
-        const y = reverse ? h * 16 : canvasHeight - (h + 1) * 16;
-        const frames = levels[h];
-        for (let i = 0; i < frames.length; i++) {
-            drawFrame(frames[i], y, h < rootLevel);
-        }
-    }
-
-    return totalMarked();
-}
-
 const exportFlamegraph = () => {
-    let eventType = null
+    let eventType = null;
     if (route.query.eventType != null) {
-        eventType = GlobalVars.jfrTypes()[route.query.eventType].code
+        eventType = GlobalVars.jfrTypes()[route.query.eventType].code;
     }
 
     FlamegraphService.export(route.query.profileId, route.query.flamegraphId, eventType)
@@ -317,14 +129,16 @@ const exportFlamegraph = () => {
         <header style="text-align: left">
             <Button id="reverse" icon="pi pi-arrows-v" class="p-button-rounded p-button-info mt-2" title="Reverse" />&nbsp;
             <Button id="search" icon="pi pi-search" class="p-button-rounded p-button-info mt-2" title="Search" />&nbsp;
-            <Button icon="pi pi-file-export" class="p-button-rounded p-button-info mt-2" @click="exportFlamegraph()" title="Export" />
+            <Button icon="pi pi-file-export" class="p-button-rounded p-button-info mt-2" @click="exportFlamegraph()"
+                    title="Export" />
         </header>
         <header style="text-align: right">Produced by <a href="https://github.com/jvm-profiling-tools/async-profiler">async-profiler</a>
         </header>
         <canvas id="canvas"></canvas>
         <div id="hl"><span></span></div>
         <p id="status"></p>
-        <p id="match">Matched: <span id="matchval"></span> <span id="reset" title="Clear">&#x274c;</span></p>
+        <p id="match" style="bottom: 10px">Matched: <span id="matchval"></span> <span id="reset"
+                                                                                      title="Clear">&#x274c;</span></p>
     </div>
 
     <Toast />
@@ -378,7 +192,8 @@ a {
     background-color: #ffffe0;
     outline: 1px solid #ffc000;
     font: 12px Arial;
-    height: 15px;
+    height: 20px;
+    padding-top: 3px;
 }
 
 #hl span {
