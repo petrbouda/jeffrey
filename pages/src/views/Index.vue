bbde7f00c3153d988ac231232e251ec00dbe4b4e
petrbouda <petr.bouda@second-foundation.eu>
2024-03-29 15:43:33 +0100

    Readme.md header

--- pages/src/views/Index.vue
+++ pages/src/views/Index.vue
@@ -23,17 +23,18 @@ const moveTo = (targetSubPage) => {
 </script>
 
 <template>
-  <div class="grid grid-nogutter bg-white border-round mb-4">
+  <div class="grid grid-nogutter bg-white border-round mb-4 p-5">
     <div class="col-6 lg:col-7 pl-6 p-2 text-center flex align-items-center flex-wrap">
-      <div class="col-12">
+      <div  class="col-6"></div>
+      <div class="col-6">
         <div class="col-12 font-bold flex justify-content-start">
           <img src="/quotation_marks.png" style="width: 15px; height: auto" class="rotate-180" alt=""/>
         </div>
 
-        <div class="font-semibold font-italic p-3 text-2xl">
+        <p class="font-semibold font-italic p-3 text-2xl leading-14">
           Hi, I'm Jeffrey, and love unraveling the mysteries of code performance and diving deep into the intricate
           layers of Java apps with Java Flight Recorder.
-        </div>
+        </p>
 
         <div class="col-12 font-bold flex align-items-center justify-content-end">
           <img src="/quotation_marks.png" style="width: 15px; height: auto" alt=""/>
@@ -44,10 +45,10 @@ const moveTo = (targetSubPage) => {
       <!--          Let's start learning new stuff together by selecting a profile, or generating a new one from recordings.-->
       <!--        </div>-->
 
-      <div class="col-12">
-        <Button label="Select a Primary Profile" type="button" :class="{ 'p-button-raised' : activePage === 'profiles', 'p-button-outlined' : activePage === 'recordings'}" class="mr-3" @click="moveTo('profiles')"></Button>
-        <Button label="Generate from Recordings" type="button" :class="{ 'p-button-raised' : activePage === 'recordings', 'p-button-outlined' : activePage === 'profiles'}" @click="moveTo('recordings')"></Button>
-      </div>
+<!--      <div class="col-12">-->
+<!--        <Button label="Select a Primary Profile" type="button" :class="{ 'p-button-raised' : activePage === 'profiles', 'p-button-outlined' : activePage === 'recordings'}" class="mr-3" @click="moveTo('profiles')"></Button>-->
+<!--        <Button label="Generate from Recordings" type="button" :class="{ 'p-button-raised' : activePage === 'recordings', 'p-button-outlined' : activePage === 'profiles'}" @click="moveTo('recordings')"></Button>-->
+<!--      </div>-->
     </div>
 
     <div class="col-6 lg:col-5 overflow-hidden">
