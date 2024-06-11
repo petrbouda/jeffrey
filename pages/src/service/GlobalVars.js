0effb69be82725feeb9b5e4f7a49092f40b0fee5
petrbouda <petr.bouda@second-foundation.eu>
2024-06-11 20:49:11 +0200

    change global vars

--- pages/src/service/GlobalVars.js
+++ pages/src/service/GlobalVars.js
@@ -1,4 +1,7 @@
 export default class GlobalVars {
-    static url = '';
+    static url = import.meta.env.DEV
+        ? 'http://localhost:8585'
+        : ''
+
     static SAP_EVENT_LINK = 'https://sap.github.io/SapMachine/jfrevents/23.html'
 }
