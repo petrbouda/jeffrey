164c3256fb2d47953daa51ac62f83f2d3f057b36
petrbouda <petr.bouda@second-foundation.eu>
2024-10-12 13:06:46 +0200

    Introduction of Guardian + huge refactoring

---
+++ service/core/src/main/java/pbouda/jeffrey/resources/JerseyConfig.java
@@ -0,0 +1,49 @@
+/*
+ * Jeffrey
+ * Copyright (C) 2024 Petr Bouda
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pbouda.jeffrey.resources;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JerseyConfig extends ResourceConfig {
+
+    public JerseyConfig() {
+        register(ProjectsResource.class);
+        register(JacksonFeature.class);
+        register(CORSFilter.class);
+    }
+
+    public static class CORSFilter implements ContainerResponseFilter {
+
+        @Override
+        public void filter(ContainerRequestContext request, ContainerResponseContext response) {
+            response.getHeaders().add("Access-Control-Allow-Origin", "*");
+            response.getHeaders().add("Access-Control-Allow-Headers",
+                    "CSRF-Token, X-Requested-By, Authorization, Content-Type, Accept, Origin");
+            response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+            response.getHeaders().add("Access-Control-Allow-Methods",
+                    "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        }
+    }
+}
---
+++ service/core/src/main/java/pbouda/jeffrey/resources/project/profile/SubSecondResource.java
@@ -0,0 +1,38 @@
+/*
+ * Jeffrey
+ * Copyright (C) 2024 Petr Bouda
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pbouda.jeffrey.resources.project.profile;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import jakarta.ws.rs.POST;
+import pbouda.jeffrey.resources.request.GetSubSecondRequest;
+import pbouda.jeffrey.manager.SubSecondManager;
+
+public class SubSecondResource {
+
+    private final SubSecondManager subSecondManager;
+
+    public SubSecondResource(SubSecondManager subSecondManager) {
+        this.subSecondManager = subSecondManager;
+    }
+
+    @POST
+    public JsonNode generate(GetSubSecondRequest request) {
+        return subSecondManager.generate(request.eventType(), request.useWeight());
+    }
+}
