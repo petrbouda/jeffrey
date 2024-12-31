9b8472f4c7b18dccbd568d1278668528a640b23b
pbouda <bouda.petr@gmail.com>
2024-12-31 08:42:02 +0100

    Single threaded profile initialization for batch upload of recordings

--- docker/build-publish-examples.sh
+++ docker/build-publish-examples.sh
@@ -1,4 +1,4 @@
 VERSION=$1
-docker build . -t petrbouda/jeffrey-examples:"$VERSION" -f Dockerfile-examples
+docker build . --no-cache -t petrbouda/jeffrey-examples:"$VERSION" -f Dockerfile-examples
 docker tag petrbouda/jeffrey-examples:"$VERSION" petrbouda/jeffrey-examples:latest
 #docker push petrbouda/jeffrey-examples:"$VERSION" petrbouda/jeffrey-examples:latest
