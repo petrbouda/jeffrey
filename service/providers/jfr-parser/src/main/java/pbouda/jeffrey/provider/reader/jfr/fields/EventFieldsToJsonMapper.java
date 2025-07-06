a4be0002a5de49fcc21c5ca449e0cc4f55b3a872
petrbouda <petr.bouda@second-foundation.eu>
2025-07-06 16:26:40 +0200

    Revert "Refactor thread models; replace thread name with ThreadInfo object in AllocatingThread and ThreadWithCpuLoad, update ProfileThreads.vue for new data structure, and enhance API query handling for improved thread metrics"
    This reverts commit 742c64736b72d41ad0bdc6363afce8a9dfd890cc.

--- service/providers/jfr-parser/src/main/java/pbouda/jeffrey/provider/reader/jfr/fields/EventFieldsToJsonMapper.java
+++ service/providers/jfr-parser/src/main/java/pbouda/jeffrey/provider/reader/jfr/fields/EventFieldsToJsonMapper.java
@@ -19,7 +19,8 @@
 package pbouda.jeffrey.provider.reader.jfr.fields;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import jdk.jfr.*;
+import jdk.jfr.EventType;
+import jdk.jfr.ValueDescriptor;
 import jdk.jfr.consumer.RecordedClass;
 import jdk.jfr.consumer.RecordedEvent;
 import jdk.jfr.consumer.RecordedMethod;
@@ -37,11 +38,6 @@ public class EventFieldsToJsonMapper implements EventFieldsMapper {
 
     public static final List<String> IGNORED_FIELDS = List.of("stackTrace");
 
-    private static final String TIMESTAMP_TYPE_NAME = Timestamp.class.getTypeName();
-    private static final String PERCENTAGE_TYPE_NAME = Percentage.class.getTypeName();
-    private static final String TIMESPAN_TYPE_NAME = Timespan.class.getTypeName();
-    private static final String UNSIGNED_TYPE_NAME = Unsigned.class.getTypeName();
-
     private final Map<Long, EventType> eventTypes = new HashMap<>();
 
     @Override
@@ -54,12 +50,16 @@ public class EventFieldsToJsonMapper implements EventFieldsMapper {
         ObjectNode node = Json.createObject();
         for (ValueDescriptor field : event.getFields()) {
             if (!IGNORED_FIELDS.contains(field.getName())) {
-                if (handleByAnnotation(field, event, node)) {
-                    // Handled by annotation, skip further processing
-                    continue;
-                }
-
-                if ("java.lang.Thread".equals(field.getTypeName())) {
+                if ("long".equals(field.getTypeName()) && "jdk.jfr.Timestamp".equals(field.getContentType())) {
+                    Instant instant = event.getInstant(field.getName());
+                    node.put(field.getName(), safeToLongMillis(instant));
+                } else if ("jdk.jfr.Percentage".equals(field.getContentType())) {
+                    float value = event.getFloat(field.getName());
+                    node.put(field.getName(), value);
+                } else if ("jdk.jfr.Timespan".equals(field.getContentType())) {
+                    Duration value = event.getDuration(field.getName());
+                    node.put(field.getName(), safeDurationToLongNanos(value));
+                } else if ("java.lang.Thread".equals(field.getTypeName())) {
                     RecordedThread value = event.getThread(field.getName());
                     node.put(field.getName(), safeThreadToString(value));
                 } else if ("java.lang.Class".equals(field.getTypeName())) {
@@ -68,7 +68,8 @@ public class EventFieldsToJsonMapper implements EventFieldsMapper {
                 } else if ("jdk.types.Method".equals(field.getTypeName())) {
                     RecordedMethod method = event.getValue(field.getName());
                     node.put(field.getName(), method.getType().getName() + "#" + method.getName());
-                } else if ("jdk.ActiveSetting".equals(event.getEventType().getName()) && "id".equals(field.getName())) {
+                } else if ("jdk.ActiveSetting".equals(event.getEventType().getName())
+                        && "id".equals(field.getName())) {
                     long eventId = event.getValue(field.getName());
                     node.put(field.getName(), eventId);
                     node.put("label", activeSettingValue(eventId));
@@ -86,30 +87,6 @@ public class EventFieldsToJsonMapper implements EventFieldsMapper {
         }
 
         return node;
-    }
-
-    private static boolean handleByAnnotation(ValueDescriptor field, RecordedEvent event, ObjectNode node) {
-        for (AnnotationElement annotation : field.getAnnotationElements()) {
-            String typeName = annotation.getTypeName();
-            if (typeName.equals(TIMESTAMP_TYPE_NAME)) {
-                Instant instant = event.getInstant(field.getName());
-                node.put(field.getName(), safeToLongMillis(instant));
-                return true;
-            } else if (typeName.equals(PERCENTAGE_TYPE_NAME)) {
-                float value = event.getFloat(field.getName());
-                node.put(field.getName(), value);
-                return true;
-            } else if (typeName.equals(TIMESPAN_TYPE_NAME)) {
-                Duration value = event.getDuration(field.getName());
-                node.put(field.getName(), safeDurationToLongNanos(value));
-                return true;
-            } else if (typeName.equals(UNSIGNED_TYPE_NAME)) {
-                long value = event.getLong(field.getName());
-                node.put(field.getName(), value);
-                return true;
-            }
-        }
-        return false;
     }
 
     private String activeSettingValue(long eventId) {
