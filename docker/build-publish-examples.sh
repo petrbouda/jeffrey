e66dd134078b2a9dc6c93cf65f808eb8c43d9d4e
petrbouda <petr.bouda@second-foundation.eu>
2025-07-27 17:34:27 +0200

    Refactor Dockerfile and build script for improved version handling; enhance error messages in InitContainerScript

--- docker/build-publish-examples.sh
+++
@@ -1,5 +0,0 @@
-VERSION=$1
-docker build . --no-cache -t petrbouda/jeffrey-examples:"$VERSION" -f Dockerfile-examples
-docker tag petrbouda/jeffrey-examples:"$VERSION" petrbouda/jeffrey-examples:latest
-docker push petrbouda/jeffrey-examples:"$VERSION"
-docker push petrbouda/jeffrey-examples:latest
