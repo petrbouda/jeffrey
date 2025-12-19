36399b32812c5e8d866e065276a53c31c472b237
Petr Bouda <petr.bouda@second-foundation.eu>
2025-12-19 08:57:42 +0100

    Add simulate resource endpoint to RootInternalResource

---
+++ service/core/src/main/java/pbouda/jeffrey/resources/SimulateResource.java
@@ -0,0 +1,38 @@
+/*
+ * Jeffrey
+ * Copyright (C) 2025 Petr Bouda
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pbouda.jeffrey.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimulateResource {
+
+    @GET
+    @Path("/oom")
+    public void triggerOutOfMemory() {
+        List<byte[]> memoryHog = new ArrayList<>();
+        while (true) {
+            // Allocate 10MB chunks until OOM
+            memoryHog.add(new byte[10 * 1024 * 1024]);
+        }
+    }
+}
